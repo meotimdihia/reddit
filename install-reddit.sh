c5ccbc3d1d12c3ccb482f95b89b4abac0fcf221b
Dien Vu <meotimdihia@gmail.com>
2014-10-30 17:37:25 +0700

    Update install-reddit.sh

--- install-reddit.sh
+++ install-reddit.sh
@@ -214,8 +214,8 @@ function clone_reddit_plugin_repo {
     clone_reddit_repo $1 reddit/reddit-plugin-$1
 }
 
-clone_reddit_repo reddit reddit/reddit
-clone_reddit_repo i18n reddit/reddit-i18n
+clone_reddit_repo reddit meotimdihia/reddit
+clone_reddit_repo i18n meotimdihia/reddit-i18n
 for plugin in $REDDIT_PLUGINS; do
     clone_reddit_plugin_repo $plugin
 done
